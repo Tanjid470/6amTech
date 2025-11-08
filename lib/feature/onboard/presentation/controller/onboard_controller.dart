b94550464ad200d43ba3592711656498a5ff9cba
Tanjid1470 <tanjid@tutorsplan.com>
2025-11-08 10:53:35 +0600

    [ADDED]: Add OnBoardRepository for fetching banner data

---
+++ lib/feature/onboard/presentation/controller/onboard_controller.dart
@@ -0,0 +1,23 @@
+import 'package:get/get.dart';
+import 'package:six_am_tech_task/feature/onboard/data/model/banners_response_body.dart';
+import 'package:six_am_tech_task/feature/onboard/data/repository/onboard_repository.dart';
+
+
+class OnBoardController extends GetxController {
+  OnBoardRepository onBoardRepository = OnBoardRepository();
+
+  List<Banners>? allBanners = [];
+
+  RxBool isLoadingMyCourseList = false.obs;
+  RxBool isLoadingMyCourseModuleList = false.obs;
+
+  Future<void> getAllBanners() async {
+    isLoadingMyCourseList.value = false;
+    final result = await onBoardRepository.getBanners();
+    if (result != null) {
+      isLoadingMyCourseList.value = true;
+      allBanners = result;
+    }
+    isLoadingMyCourseList.value = true;
+  }
+}
