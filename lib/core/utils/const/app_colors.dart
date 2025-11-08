e5300a3a726d4df1f2bea961ed36f2289bc10f64
Tanjid1470 <tanjid@tutorsplan.com>
2025-11-08 10:10:40 +0600

    [REFACTORED]: Rename AppColor to AppColors and update baseColor

--- lib/core/utils/const/app_colors.dart
+++ lib/core/utils/const/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class AppColor{
-  AppColor._();
-  static Color baseColor = const Color(0xff0346ae);
+class AppColors{
+  AppColors._();
+  static Color baseColor = const Color(0xff2b7a56);
 
 }
