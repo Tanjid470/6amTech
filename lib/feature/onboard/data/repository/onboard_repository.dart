95d9787405bbcacf5ccd245c1e979aa1ae22326c
Tanjid1470 <tanjid@tutorsplan.com>
2025-11-08 10:53:32 +0600

    [ADDED]: Add OnBoardRepository for fetching banner data

---
+++ lib/feature/onboard/data/repository/onboard_repository.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:six_am_tech_task/core/utils/const/url_conts.dart';
+import 'package:six_am_tech_task/core/utils/network/api_client.dart';
+import 'package:six_am_tech_task/core/utils/network/dio_header.dart';
+import 'package:six_am_tech_task/feature/onboard/data/model/banners_response_body.dart';
+
+class OnBoardRepository {
+  Dio dio = Dio();
+
+  Future<List<Banners>?> getBanners() async {
+    try {
+      dio = await ApiClient.dioClient(true);
+      Response response = await dio.get(
+        UrlConst.banners,
+        options: dioHeader(),
+      );
+      BannersResponse data = BannersResponse.fromJson(response.data);
+      if (response.statusCode == 200) {
+        return data.banners?.toList();
+      }
+      else {
+        return null;
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+    return null;
+  }
+
+}
